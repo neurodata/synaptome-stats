7d0ba8ef7156444cde95957e0b5df37e247bf7d7
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2016-06-03 17:20:36 -0400

    Playing with kmeans++

---
+++ Code/kmpp.r
@@ -0,0 +1,54 @@
+#' K-Means++ initialization to K-means
+#'
+#' INPUTS:
+#' x: The data formatted as a matrix.
+#' k: The number of clusters desired. \code{k < nrows(x)}
+#' 
+#'
+#' REFERNCE:
+#' Arthur, David and Vassilvitskii, Sergei k-means++: The Advantages of Careful Seeding
+#'
+
+
+kmpp <- function(x,k=2){
+
+la2 <- function(a,b){
+        m1 <- matrix(as.numeric(a),nrow=dim(b)[1],ncol=dim(b)[2], byrow=TRUE)
+    
+        if(all(dim(m1) == dim(b))){
+            dif <- apply((m1-b)^2,1,sum)    
+        return(dif)
+        } else {
+            stop("Wrong dimensions!")}
+        }
+
+### Step 1a
+#set.seed(2^13)
+s1 <- cp <- sample(nrow(x),1)
+
+ind <- c(cp)
+
+centers <- list()
+
+centers[[1]] <- x[s1,]
+
+p <- la2(x[s1,],x)
+P <- p/sum(p) ## P = D(x')^2/( ∑_x\inX  D(x)^2)
+
+### Step 1b-c
+for( i in 2:k ) {
+    #set.seed(2^13)
+    cp <- sample(nrow(x),1,prob=P) ## c'
+    centers[[i]] <-x[cp,]
+    ind <- c(ind,cp)
+    
+    p <- apply(cbind(la2(x[cp,],x),p),1,min) #apply(cbind(p,ds[,cp]),1,min)
+    P <- p/sum(p)
+    }
+
+Ctrs <- Reduce('rbind',centers)
+
+kObj <- kmeans(x, centers=Ctrs)
+
+return(kObj)
+} ### END FUNCTION
