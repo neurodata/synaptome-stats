2750467e2f790c7d6fc7be62b0931ee30e7ed3c7
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2016-06-15 14:56:25 -0400

    Fixed kmpp for hierarcical k=2 clustering [neurodata/weekly-experiments#95]

--- Code/kmpp.r
+++ Code/kmpp.r
@@ -52,3 +52,29 @@ kObj <- kmeans(x, centers=Ctrs)
 
 return(kObj)
 } ### END FUNCTION
+
+
+bhkmpp <- function(x,blevels){
+    require(data.table)
+
+    k0 <- kmpp(x,2)
+    L <- data.table(lv1=k0$cluster)
+
+    for(y in 2:blevels){ L[[y]] <- 0L }
+    colnames(L) <- paste0("lv", 1:blevels)
+    
+    for(j in 1:(blevels-1)){
+    for(i in sort(unique(L[[j]]))){
+        ## Set seed to keep cluster labels somewhat consistant 
+        set.seed(13)
+        kv <- kmpp(fs[L[[j]] == i,], k = 2)
+        if(i != 1){kv$cluster <- as.integer(kv$cluster+i)}
+        L[L[[j]]==i,][[j+1]] <- kv$cluster
+        }
+    }
+    return(L)
+}
+
+
+
+
