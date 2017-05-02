c1833a9d93934540587cb23e4e53344d1bb4cf23
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2017-05-02 14:52:03 -0400

    Add Weiler stackM plots.

--- Code/Python/runAll.sh
+++ Code/Python/runAll.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+python getF0.py Ex10R55 data/Ex10R55/resultVol_1.csv resultVol_1 &
+python getF0.py Ex10R55 data/Ex10R55/resultVol_2.csv resultVol_2 &
+python getF0.py Ex10R55 data/Ex10R55/resultVol_3.csv resultVol_3 &
+wait
+python getF0.py Ex10R55 data/Ex10R55/resultVol_6.csv resultVol_6 &
+python getF0.py Ex10R55 data/Ex10R55/resultVol_7.csv resultVol_7 &
+python getF0.py Ex10R55 data/Ex10R55/resultVol_8.csv resultVol_8 &
+python getF0.py Ex10R55 data/Ex10R55/resultVol_9.csv resultVol_9 &
+wait
+python getF0.py Ex12R75 data/Ex12R75/resultVol_1.csv resultVol_1 &
+python getF0.py Ex12R75 data/Ex12R75/resultVol_2.csv resultVol_2 &
+python getF0.py Ex12R75 data/Ex12R75/resultVol_3.csv resultVol_3 &
+wait
+python getF0.py Ex12R75 data/Ex12R75/resultVol_6.csv resultVol_6 &
+python getF0.py Ex12R75 data/Ex12R75/resultVol_7.csv resultVol_7 &
+python getF0.py Ex12R75 data/Ex12R75/resultVol_8.csv resultVol_8 &
+python getF0.py Ex12R75 data/Ex12R75/resultVol_9.csv resultVol_9 &
+wait
 python getF0.py Ex12R76 data/Ex12R76/resultVol_1.csv resultVol_1 &
 python getF0.py Ex12R76 data/Ex12R76/resultVol_2.csv resultVol_2 &
 python getF0.py Ex12R76 data/Ex12R76/resultVol_3.csv resultVol_3 &
--- Code/Python/runEx2R18C1.sh
+++ Code/Python/runEx2R18C1.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 python getF0.py Ex2R18C1 data/Ex2R18C1/resultVol_1.csv resultVol_1 &
 python getF0.py Ex2R18C1 data/Ex2R18C1/resultVol_2.csv resultVol_2 &
-python getF0.py Ex2R18C1 data/Ex2R18C1/resultVol_3.csv resultVol_3 &
 wait
+python getF0.py Ex2R18C1 data/Ex2R18C1/resultVol_3.csv resultVol_3 &
 python getF0.py Ex2R18C1 data/Ex2R18C1/resultVol_6.csv resultVol_6 &
+wait
 python getF0.py Ex2R18C1 data/Ex2R18C1/resultVol_7.csv resultVol_7 &
 python getF0.py Ex2R18C1 data/Ex2R18C1/resultVol_8.csv resultVol_8 &
+wait
 python getF0.py Ex2R18C1 data/Ex2R18C1/resultVol_9.csv resultVol_9 &
