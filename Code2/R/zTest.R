7c55afd7d5507e6a3eb3d3246da8b86b281e3960
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2017-05-12 19:42:21 -0400

    code from today, @jovo

--- Code2/R/zTest.R
+++ Code2/R/zTest.R
@@ -2,28 +2,18 @@ suppressPackageStartupMessages(require(rhdf5))
 suppressPackageStartupMessages(require(meda))
 require(gridExtra)
 require(foreach)
+require(raster)
 
-fname <- "../Python/kristina15rscaled317samp5000_synaptograms11cubes5NN_test4.h5"
-
-hmcO <- readRDS("run10/data/outputs/K15F0_rscale317samp5000/hmc.rds")
-clusterN <- hmcO$dat$Get("name", filterFun = isLeaf)
+fname <- "../Python/kristina15rscaled317samp5000_synaptograms11cubes5NN_test7.h5"
 
 h5ls(fname) 
 cnames <- h5read(fname, name = "Channels")
 abcnames <- abbreviate(gsub("_", "", cnames), minlength = 3)
 dat <- h5read(fname, name = "kristina15")
-dimnames(dat) <- list(1:40, cnames, NULL, NULL, NULL)
+dimnames(dat) <- list(1:dim(dat)[1], cnames, NULL, NULL, NULL)
 datO <- dat
 loc <- h5read(fname, name = "Locations")
-trueLoc <- as.character(clusterN[read.csv("K15F0_rscaled317samp5e3_hmc5NN.csv", header = TRUE)[,4]])
-#cseq <- rep(1:10,each =5)
-#clusterN <- hmcO$dat$Get("name", filterFun = isLeaf)
-#trueLoc <- cbind(trueLoc, cseq)
-#trueCl <- trueLoc[trueLoc$z >=5, 'cseq']
-
-
 H5close()
-
 
 type <- c("in", "ex", "in", "ot", 
           "in", "ex", "ot", "in", 
@@ -51,15 +41,43 @@ for(j in 1:dim(dat)[2]){
     }
   }
 
+F0 <- 
+  foreach(j = 1:dim(dat)[1]) %:%
+  foreach(i = 1:dim(dat)[2]) %do% {
+      f0 <- sum(dat[j,i,,,])
+}
+
+names(F0) <- 1:length(F0)
+for(i in 1:length(F0)){
+  names(F0[[i]]) <- cnames
+}
+
+j <- dat
 
 rr <- foreach(l = 1:dim(dat)[1]) %do% {
   r <- dat[l,,,,]
   mr <- melt(r)
-  names(mr) <- c("chN", "x", "y", "z", "value")
+  names(mr) <- c("ch", "x", "y", "z", "value")
   mr$z <- mr$z - (max(mr$z) +1)/2
-  mr$ch <- cnames[mr$chN]
-  mr$type <- ctype$type[mr$chN]
-  mr
+
+  F0z <- Reduce('rbind', 
+           lapply(names(F0[[l]]), 
+             function(n){
+             Fz <- mr[mr$ch == n,]
+             #Fz$z <- as.numeric(max(mr$z)+1)
+             Fz$F0 <- round(as.numeric(F0[[l]][n]),4)
+             Fz
+             }))
+
+  #mr <- rbind(mr, F0z)
+  #ch <- mr$ch
+  #mr$type <- ctype$type[ch]
+  #mr$ch <- abcnames[ch]
+  #mr
+  ch <- F0z$ch
+  F0z$type <- ctype$type[ch]
+  F0z$ch <- abcnames[ch]
+  F0z
 }
 
 
@@ -67,6 +85,7 @@ names(rr) <- 1:length(rr)
 
 th <- theme(axis.text = element_blank(), axis.ticks = element_blank(),
             axis.title.y = element_blank(), axis.title.x = element_blank(),
+            legend.position="bottom", legend.key.size = unit(2,'lines'),
             panel.spacing = unit(0, "lines"))
 
 #registerDoMC(6)
@@ -78,7 +97,8 @@ MMin <- max(sapply(rr, function(am){ max(am[am$type == "in",]$value) }))
 mmot <- min(sapply(rr, function(am){ min(am[am$type == "ot",]$value) }))
 MMot <- max(sapply(rr, function(am){ max(am[am$type == "ot",]$value) }))
 
-for(k in 1:length(rr)){
+#for(k in 1:length(rr)){
+for(k in 1:5){
   mr <- rr[[k]]
 #mm <- 0
 #MM <- 1
@@ -87,65 +107,40 @@ ggplot(mr[mr$type == "ex",],
   aes(x,y, group = factor(type), fill = value)) +
   geom_raster() + 
   scale_y_reverse() + 
-  facet_grid(ch ~ z, labeller = label_both) +
-  scale_fill_gradient(low = "black", high = "green", limits = c(mmex,MMex)) + th
+  facet_grid(ch+F0 ~ z, labeller = label_both) +
+  #scale_fill_gradient(low = "black", high = "green", limits = c(mmex,MMex)) + th
+  scale_fill_gradient(low = "black", high = "green") + th
 
 pin <- 
 ggplot(mr[mr$type == "in",], 
   aes(x,y, group = factor(type), fill = value)) +
   geom_raster() + 
   scale_y_reverse() + 
-  facet_grid(ch ~ z, labeller = label_both) +
-  scale_fill_gradient(low = "black", high = "red", limits = c(mmin,MMin)) + th
+  facet_grid(ch+F0 ~ z, labeller = label_both) +
+  #scale_fill_gradient(low = "black", high = "red", limits = c(mmin,MMin)) + th
+  scale_fill_gradient(low = "black", high = "red") + th
 
 pot <- 
   ggplot(mr[mr$type == "ot",], 
   aes(x,y, group = factor(type), fill = value)) +
   geom_raster() +
   scale_y_reverse() + 
-  facet_grid(ch ~ z, labeller = label_both) +
-  scale_fill_gradient(low = "black", high = "blue", limits = c(mmot,MMot)) + th
+  facet_grid(ch+F0 ~ z, labeller = label_both) +
+  #scale_fill_gradient(low = "black", high = "blue", limits = c(mmot,MMot)) + th
+  scale_fill_gradient(low = "black", high = "blue") + th
 #
 
-lay <- rbind(matrix(1,11,23), 
-      matrix(2,8,23), 
-      matrix(3,4,23))
+lay <- rbind(matrix(1,11,1), 
+      matrix(2,8,1), 
+      matrix(3,4,1))
 
 
 
-outName <- paste("~/Desktop/k15syn_log10scale",
-           paste0("C", trueLoc[k]), paste(loc[k,], collapse = "_"), sep = "_", collapse = "_") 
+outName <- paste("~/Desktop/zTest",
+           paste0("C",paste(loc[k,], collapse = "_"), sep = "_", collapse = "_") )
 png(paste0(outName, ".png"), 
     height = 23*1.5, width = 11*1.5, units = "in", res = 300)
 print(grid.arrange(pex, pin, pot, layout_matrix = lay))
 dev.off()
 }
-
-
-cisbaseZ <- "http://cis.jhu.edu/~jesse/fotd/20170509/synaptograms/k15syn_log10scale"
-cisbase0 <- "http://cis.jhu.edu/~jesse/fotd/20170509/synaptograms/k15syn_log10scale"
-ndviz <- "http://viz.neurodata.io/project/kristina15_SynDiv_JLP/xy/0/"
-
-
-out <- foreach(k = 1:length(rr), .combine = 'rbind')%do%{
- outNameZ <- paste(cisbaseZ,
-            paste0("C", trueLoc[k]), paste(loc[k,], collapse = "_"), sep = "_", collapse = "_") 
- 
- outName0 <- paste(cisbase0,
-            paste0("C", trueLoc[k]), paste(loc[k,], collapse = "_"), sep = "_", collapse = "_") 
-
- cl <- paste0("C", trueLoc[k], "_", paste(loc[k,], collapse = "_"))
-
- cluZ <-  paste0(outNameZ, ".png")
- clu0 <-  paste0(outName0, ".png")
-
- nd <- paste0(ndviz, paste(loc[k,1], loc[k,2], loc[k,3], sep = "/"))
-
- sprintf("[rscale%s](%s) \t\t| [01scale%s](%s) \t\t| [viz%s](%s/)  ",cl, cluZ, cl, clu0, cl, nd)
-}
-
-write.table(out, file = "~/Desktop/links.txt", quote = FALSE, row.names = FALSE)
-
-
-
 
--- data/Anish_csvfiles/reformatCSV2xyz.R
+++ data/Anish_csvfiles/reformatCSV2xyz.R
@@ -10,8 +10,10 @@ for(i in dir0){
  csvf <- dir(exdir, full.names=TRUE)
  for(j in csvf){
    r <- read.csv(j, header = FALSE)
+   ## Translating from (row, col, z) to (x,y,z)
    n <- paste0("XYZ/", strsplit(j, "\\.")[[1]][1], "xyz.csv")
-   out <- r[, c(2,1,3)]
+   ## Translating to indexing from 0
+   out <- r[, c(2,1,3)] - 1
    colnames(out) <- c("col", "row", "z")
    write.csv(out, file = n, row.names = FALSE)
  }
