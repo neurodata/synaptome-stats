50addd06f0e364426cb343c1c5de449fafc0226e
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2017-07-11 22:01:02 -0400

    betterment

--- data/Anish_csvfiles/XYZ/aggregateLocationsXYZ.R
+++ data/Anish_csvfiles/XYZ/aggregateLocationsXYZ.R
@@ -7,7 +7,6 @@ basedir <- getwd()
 dir0 <- grep("^Ex", dir(), value = TRUE)
 dir0
 
-d1 <- list(list())
 d0 <- list()
 
 for(i in 1:length(dir0)){
@@ -20,9 +19,11 @@ for(i in 1:length(dir0)){
 
   for(j in 1:length(subd)){
     d0[[length(d0)+1]] <- 
-      as.data.table(cbind(Ex = Ex, Q = vol[[j]], read.csv(subd[[j]])))
+      as.data.table(cbind(read.csv(subd[[j]]), Ex = Ex, Q = vol[[j]]))
   }
 }
+
+
 DFull <- as.data.table(Reduce("rbind", d0))
 
 ExLev <- levels(DFull$Ex)
@@ -37,4 +38,23 @@ h5write(ExLev, fname, "Levels/Ex")
 h5write(Qlev, fname, "Levels/Q")
 h5ls(fname)
 H5close()
+
+ex10r55 <- DFull[Ex == "Ex10R55" & col > 4 & row > 4 & z > 4 & col < max(col)-5 & row < max(row)-5 & z < max(z) - 4,]
+ex10r55[, Ex := NULL]
+
+ex10r55[, `:=`(col = round(col), # rounding to nearest integer
+               row = round(row),
+               z   = round(z))]
+
+fname <- "Ex10R55_BufferedPM5.h5"
+clean <- h5createFile(fname)
+#clean <- h5createGroup(fname, "Locations")
+h5write(ex10r55, fname, "Locations")
+h5ls(fname)
+H5close()
+
+set.seed(317)
+s <- sample(nrow(ex10r55))
+write.csv(ex10r55[s,1:3], file = "ex10r55_buffered_seed317.csv", row.names = FALSE)
+
 
